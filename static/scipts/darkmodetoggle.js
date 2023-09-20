8b751b303f9140065c8e2beac49d758c9807d20d
Elias Hawa <elias.hawa2468@gmail.com>
2023-09-19 21:08:10 -0400

    Fix project items dark mode toggle

--- static/scipts/darkmodetoggle.js
+++ static/scipts/darkmodetoggle.js
@@ -1,9 +1,16 @@
 function toggleDarkMode() {
   var darkToggle = document.getElementById("dark-mode-check-box");
   var body = document.body;
+  var projectItems = document.getElementsByClassName("project-item");
   if (darkToggle.checked) {
     body.style.animation = "fade-to-dark 1s forwards";
+    for (var i = 0; i < projectItems.length; i++) {
+      projectItems[i].style.animation = "fade-to-dark-project-item 1s forwards";
+    }
   } else {
     body.style.animation = "fade-to-light 1s forwards";
+    for (var i = 0; i < projectItems.length; i++) {
+      projectItems[i].style.animation = "fade-to-light-project-item 1s forwards";
+    }
   }
 }
