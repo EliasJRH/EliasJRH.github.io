a20d0c57161222ae3f56ad1588f35f03e782bf01
Elias Hawa <elias.hawa2468@gmail.com>
2023-10-01 10:09:47 -0400

    Finalize timeline darktheme changes

--- static/scripts/darkmodetoggle.js
+++ static/scripts/darkmodetoggle.js
@@ -2,15 +2,19 @@ function toggleDarkMode() {
   var darkToggle = document.getElementById("dark-mode-check-box");
   var body = document.body;
   var projectItems = document.getElementsByClassName("project-item");
-  var flags = document.getElementsByClassName("flag");
+  var timelineFlags = document.getElementsByClassName("flag");
+  var timelineDescs = document.getElementsByClassName("desc");
   var flagsQS = document.querySelectorAll(".flag");
   if (darkToggle.checked) {
     body.style.animation = "fade-to-dark 1s forwards";
     for (var i = 0; i < projectItems.length; i++) {
       projectItems[i].style.animation = "fade-to-dark-project-item 1s forwards";
     }
-    for (var i = 0 ; i < flags.length; i++) {
-      flags[i].style.animation = "fade-to-dark-timeline-flag 1s forwards";
+    for (var i = 0 ; i < timelineFlags.length; i++) {
+      timelineFlags[i].style.animation = "fade-to-dark-timeline-flag 1s forwards";
+    }
+    for (var i = 0; i < timelineDescs.length; i++) {
+      timelineDescs[i].style.animation = "fade-to-dark-timeline-desc 1s forwards";
     }
     for (var i = 0 ; i < flagsQS.length; i++) {
       flagsQS[i].classList.toggle("light");
@@ -20,8 +24,11 @@ function toggleDarkMode() {
     for (var i = 0; i < projectItems.length; i++) {
       projectItems[i].style.animation = "fade-to-light-project-item 1s forwards";
     }
-    for (var i = 0 ; i < flags.length; i++) {
-      flags[i].style.animation = "fade-to-light-timeline-flag 1s forwards";
+    for (var i = 0 ; i < timelineFlags.length; i++) {
+      timelineFlags[i].style.animation = "fade-to-light-timeline-flag 1s forwards";
+    }
+    for (var i = 0; i < timelineDescs.length; i++) {
+      timelineDescs[i].style.animation = "fade-to-light-timeline-desc 1s forwards";
     }
     for (var i = 0 ; i < flagsQS.length; i++) {
       flagsQS[i].classList.toggle("light");
