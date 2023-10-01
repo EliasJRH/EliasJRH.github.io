716dc4ca6206e6896d1417495301b3fce09a23f6
Elias Hawa <elias.hawa2468@gmail.com>
2023-10-01 09:41:59 -0400

    Refactor animation names

--- static/scripts/darkmodetoggle.js
+++ static/scripts/darkmodetoggle.js
@@ -5,23 +5,23 @@ function toggleDarkMode() {
   var flags = document.getElementsByClassName("flag");
   var flagsQS = document.querySelectorAll(".flag");
   if (darkToggle.checked) {
-    body.style.animation = "fade-to-light 1s forwards";
-    for (var i = 0; i < projectItems.length; i++) {
-      projectItems[i].style.animation = "fade-to-light-project-item 1s forwards";
-    }
-    for (var i = 0 ; i < flags.length; i++) {
-      flags[i].style.animation = "fade-to-light-flag 1s forwards";
-    }
-    for (var i = 0 ; i < flagsQS.length; i++) {
-      flagsQS[i].classList.toggle("light");
-    }
-  } else {
     body.style.animation = "fade-to-dark 1s forwards";
     for (var i = 0; i < projectItems.length; i++) {
       projectItems[i].style.animation = "fade-to-dark-project-item 1s forwards";
     }
     for (var i = 0 ; i < flags.length; i++) {
-      flags[i].style.animation = "fade-to-dark-flag 1s forwards";
+      flags[i].style.animation = "fade-to-dark-timeline-flag 1s forwards";
+    }
+    for (var i = 0 ; i < flagsQS.length; i++) {
+      flagsQS[i].classList.toggle("light");
+    }
+  } else {
+    body.style.animation = "fade-to-light 1s forwards";
+    for (var i = 0; i < projectItems.length; i++) {
+      projectItems[i].style.animation = "fade-to-light-project-item 1s forwards";
+    }
+    for (var i = 0 ; i < flags.length; i++) {
+      flags[i].style.animation = "fade-to-light-timeline-flag 1s forwards";
     }
     for (var i = 0 ; i < flagsQS.length; i++) {
       flagsQS[i].classList.toggle("light");
